4da45f5b24efbdf29e36644e44905e302b364e97
Alejandro Santiago <cubanfireball81@gmail.com>
2021-11-28 15:50:57 -0900

    Finished Wine House Project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+// Set Dynamic variables
 let counter1 = 0;
 let counter2 = 1;
 let bool = true;
 
+// Set static variables
 const sections = document.querySelectorAll('section');
 const progress = document.querySelector('.progress h2');
 const circles = document.querySelectorAll('.circle');
@@ -9,17 +11,19 @@ const menu = document.querySelector('.menu');
 const section1wrapper = document.querySelector('.section-1-wrapper');
 const section5wrapper = document.querySelector('.section-5-wrapper');
 
+// Set scale for Section 1 wrapper
 section1wrapper.style.transform = "scale(1)";
 
+// Update progress circles based on counter2
 const progressCounter = () => {
     progress.textContent = `${counter2}/${sections.length}`;
-
     Array.from(circles).forEach(circle => {
         circle.style.backgroundColor = 'transparent';
     });
     document.querySelector(`.circle-${counter2}`).style.backgroundColor = '#ddd';
 };
 
+// Dynamically change scale of background image
 const pageController = () => {
     bool = true;
     if(counter1 === 5) {
@@ -33,7 +37,6 @@ const pageController = () => {
         progressCounter();
         bool = false;
     }
-
     if(counter1 === -1) {
         Array.from(sections).forEach((section) => {
             if(section.classList[0] === 'section-5') {
@@ -52,6 +55,7 @@ const pageController = () => {
     return bool;
 }
 
+// Wheel/Scroll event listener to change page +/-
 window.addEventListener('wheel', (e) => {
     const deltaY = e.deltaY > 0;
     if(deltaY > 0) {
@@ -61,15 +65,12 @@ window.addEventListener('wheel', (e) => {
         counter1--;
         counter2--;
     }
-
     pageController();
     progressCounter();
-
     if(bool) {
         document.querySelector(
         `.section-${deltaY ? counter1 : counter2}`
         ).style.left = `${deltaY ? "-100vw" : "0"}`;
-
         document.querySelector(
             `.section-${deltaY ? counter1 : counter2}-wrapper`
             ).style.transform = `scale(${deltaY ? '1.5' : '1'})`;
@@ -79,18 +80,18 @@ window.addEventListener('wheel', (e) => {
     }
 });
 
+// Left button click goes back to previous page or cycle backwards
 document.querySelector('.left-btn').addEventListener('click', () => {
     counter1--;
     counter2--;
     pageController() && 
     (document.querySelector(`.section-${counter2}`).style.left = '0');
-
     if(bool) {
         document.querySelector(`.section-${counter2}-wrapper`).style.transform = "scale(1)";
         document.querySelector(`.section-${counter2 + 1}-wrapper`).style.transform = "scale(1.5)";
     }
 });
-
+// Right button click moves ahead a page or continues to beginning
 document.querySelector('.right-btn').addEventListener('click', () => {
     counter1++;
     counter2++;
@@ -102,14 +103,17 @@ document.querySelector('.right-btn').addEventListener('click', () => {
     }
 });
 
+// Change scale of grapes img on hover (preserve img quality)
 document.querySelector('.grapes-img').addEventListener('mouseover', () => {
     document.querySelector('.section-3-wrapper').style.opacity = '.5';
 });
 
+// Change opacity of grapes img on hover
 document.querySelector('.grapes-img').addEventListener('mouseout', () => {
     document.querySelector('.section-3-wrapper').style.opacity = '1';
 });
 
+// Hamburger menu show/hide by adding change class
 menu.addEventListener('click', () => {
     document.querySelector('.navbar').classList.toggle('change');
 });
